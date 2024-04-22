bd4c638e1b5405b9d7f4ef5a93f5fc9e3bcfd8d8
Sunil Rajput <48656178+sreiker@users.noreply.github.com>
2024-04-23 01:41:26 +0530

    Update android-web-test.js

--- android-web-test.js
+++ android-web-test.js
@@ -3,19 +3,19 @@ describe("Web automation real device", () => {
   it("Opening Website", async () => {
     browser.url("https://mfml.in/api/getInfo")
     var color = await $("//input[@id='resolution']");
-    await color.waitForDisplayed({ timeout: 30000 });
+    await color.waitForDisplayed({ timeout: 50000 });
     await color.click();
     await color.click();
     windowHandles = await browser.getWindowHandles();
     console.log(windowHandles);
     var text = await $("//input[@id='location']");
-    await text.waitForDisplayed({ timeout: 30000 });
+    await text.waitForDisplayed({ timeout: 50000 });
     await text.click();
     var toast = await $("//textarea[@id='details']");
-    await toast.waitForDisplayed({ timeout: 30000 });
+    await toast.waitForDisplayed({ timeout: 50000 });
     await toast.click();
     var nf = await $("//textarea[@id='timezone']");
-    await nf.waitForDisplayed({ timeout: 30000 });
+    await nf.waitForDisplayed({ timeout: 50000 });
     await nf.click();
     driver.executeScript("lambda-status=passed",[]);
 
