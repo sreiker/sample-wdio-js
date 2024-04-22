a1e2b4a7b9cf984d4745b6445a83739ba3557ccf
Sunil Rajput <48656178+sreiker@users.noreply.github.com>
2024-04-23 00:17:55 +0530

    Add files via upload

---
+++ android-web-single-ltOptions-w3c.conf.js
@@ -0,0 +1,50 @@
+exports.config = {
+    user: process.env.LT_USERNAME || "YOUR_USERNAME",
+    key: process.env.LT_ACCESS_KEY || "YOUR_ACCESS_KEY",
+  
+    updateJob: false,
+    specs: ["android-web-test.js"],
+    exclude: [],
+  
+    // commonCapabilities: {
+    //   build: "LT_Appium_NodeJS_WebDriverIO_ltoptions_w3_Web_Automation",
+    //   visual: true,
+    // },
+  
+    capabilities: [
+      {
+        "lt:options": {
+          platformName: "Android",
+          deviceName: ".*",
+          name: "android_ltOptions_w3c",
+          isRealMobile: true,
+          enableCustomTranslation: true,
+          w3c: true,
+          build: "Build_A"
+        }
+      },
+    ],
+  
+    logLevel: "info",
+    coloredLogs: true,
+    screenshotPath: "./errorShots/",
+    baseUrl: "https://mobile-hub.lambdatest.com",
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 90000,
+    connectionRetryCount: 3,
+    path: "/wd/hub",
+    hostname: process.env.LT_GRID_URL||"mobile-hub.lambdatest.com",
+    port: 80,
+  
+    framework: "mocha",
+    mochaOpts: {
+      ui: "bdd",
+      timeout: 20000,
+    },
+  };
+  
+  exports.config.capabilities.forEach(function (caps) {
+    for (var i in exports.config.commonCapabilities)
+      caps[i] = caps[i] || exports.config.commonCapabilities[i];
+  });
+  
