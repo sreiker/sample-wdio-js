923a16faa58cf2b1140a8d48bff951c0ed380c8e
Sunil Rajput <48656178+sreiker@users.noreply.github.com>
2024-04-23 12:17:01 +0530

    Update android-web-single-ltOptions-w3c.conf.js

--- android-web-single-ltOptions-w3c.conf.js
+++ android-web-single-ltOptions-w3c.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
     user: "sunilr",//process.env.LT_USERNAME || "YOUR_USERNAME",
-    key: "LtQPqIWXiN4kHjRSQ9juzDmQZV7tpbssbvyEJ13JPS4QfhGduU"//process.env.LT_ACCESS_KEY || "YOUR_ACCESS_KEY",
+    key: "LtQPqIWXiN4kHjRSQ9juzDmQZV7tpbssbvyEJ13JPS4QfhGduU",//process.env.LT_ACCESS_KEY || "YOUR_ACCESS_KEY",
   
     updateJob: false,
     specs: ["android-web-test.js"],
